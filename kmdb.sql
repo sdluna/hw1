1c8632a09803c31a9f7a55711927c38df221f48d
Simone D'Luna <simone.dluna@kellogg.northwestern.edu>
2024-01-19 17:57:23 +0000

    Creating and dropping tables, starting database model

--- kmdb.sql
+++ kmdb.sql
@@ -105,9 +105,34 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE movies;
+DROP TABLE studios;
+DROP TABLE actors;
+DROP TABLE characters;
 
 -- Create new tables, according to your domain model
 -- TODO!
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year TEXT,
+    mpaa_rating TEXT,
+    studio_id INTEGER);
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT);
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_name TEXT,
+    actor_id INTEGER,
+    movie_id INTEGER);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
